aa54f443968b6d58a67e16035af109816d7a8739
JVxie <1136404654@qq.com>
2020-03-30 21:06:20 +0800

    初始化项目

---
+++ src/main/java/com/jvxie/goshop/GoshopApplication.java
@@ -0,0 +1,13 @@
+package com.jvxie.goshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoshopApplication.class, args);
+    }
+
+}
